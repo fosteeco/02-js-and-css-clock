cb489302b31b816da80a1cdb806aaab929e5d231
Christian Foster <fosterchris000@gmail.com>
2022-04-20 09:09:51 -0400

    Finished project

---
+++ script.js
@@ -0,0 +1,28 @@
+const secHand = document.querySelector(".hand.second-hand");
+const minHand = document.querySelector(".hand.min-hand");
+const hourHand = document.querySelector(".hand.hour-hand");
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+const spinArm = (base, elem, val) => {
+  const spinDegrees = 360 * (val / base) + 90;
+  elem.style.transform = `rotate(${spinDegrees}deg)`;
+  console.log(base, "spinDegrees :", spinDegrees);
+};
+
+const interval = setInterval(() => {
+  let d = new Date();
+  let s = addZero(d.getSeconds());
+  let m = addZero(d.getMinutes());
+  let h = addZero(d.getHours());
+  spinArm(60, secHand, s);
+  spinArm(60, minHand, m);
+  spinArm(24, hourHand, h);
+  let time = h + ":" + m + ":" + s;
+  console.log("time :", time);
+}, 1000);
